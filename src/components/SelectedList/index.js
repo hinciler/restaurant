8e2c7fbae1797659d74223bd22558ffea81d543d
Cihan BAS <cihanbas07@gmail.com>
2020-05-26 14:01:44 +0300

    Destroyed modal header

--- src/components/SelectedList/index.js
+++ src/components/SelectedList/index.js
@@ -9,37 +9,27 @@ import {colors} from 'config';
 import {Header, normalize} from 'react-native-elements';
 import _ from 'lodash';
 const styles = StyleSheet.create({
-  modalcontainer: {
+  modalContainer: {
+    flex: 1,
     justifyContent: 'flex-end',
   },
   flatListContainer: {
-    borderRadius: 10,
-    backgroundColor: 'green',
+    borderTopLeftRadius: normalize(10),
+    borderTopRightRadius: normalize(10),
     bottom: 0,
-  },
-  headerContainer: {
-    backgroundColor: 'transparent',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    borderColor: 'red',
-  },
-  contentWrapper: {
+    justifyContent: 'flex-end',
     backgroundColor: 'white',
-    borderRadius: normalize(10),
-    paddingTop: normalize(10),
+    paddingTop: normalize(20),
+    flexGrow: 1,
+    marginTop: 80,
+  },
+
+  contentWrapper: {
+    borderTopLeftRadius: normalize(10),
   },
 });
 const checklist = [];
-export default function ({
-  visible = false,
-  setVisible,
-  list,
-  headerText = 'SambaPOS',
-  onSave,
-  type,
-}) {
+export default function ({visible = false, setVisible, list, onSave, type}) {
   const [selectIndex, selectRadio] = useState(0);
   const checked = (data) => {
     const _index = _.findIndex(checklist, ['_key', data._key]);
@@ -55,18 +45,8 @@ export default function ({
       onBackdropPress={() => setVisible(false)}
       animationOut="fadeOutDown"
       hideModalContentWhileAnimating={true}
-      style={styles.modalcontainer}
+      style={styles.modalContainer}
       useNativeDriver={true}>
-      <Header
-        containerStyle={styles.headerContainer}
-        leftComponent={{
-          icon: 'close',
-          color: '#fff',
-          size: normalize(24),
-          onPress: () => setVisible(false),
-        }}
-        centerComponent={<Text text={headerText} color={colors.white} />}
-      />
       <View style={styles.contentWrapper}>
         <FlatList
           data={list}
@@ -83,7 +63,10 @@ export default function ({
               />
             )
           }
-          keyExtractor={(item) => item.id}
+          keyExtractor={(item) => {
+            console.log('item', item);
+            return item.Id;
+          }}
         />
         <Button
           text={'KAYDET'}
