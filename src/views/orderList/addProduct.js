9478b9c32e3c5da7141e251ca4195fec2ca6695a
Cihan BAS <cihanbas07@gmail.com>
2020-07-28 16:59:11 +0300

    added scrollview to addProduct Page

--- src/views/orderList/addProduct.js
+++ src/views/orderList/addProduct.js
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   Dimensions,
   TouchableHighlight,
+    ScrollView
 } from 'react-native';
 import {Actions} from 'react-native-router-flux';
 import {colors} from 'config';
